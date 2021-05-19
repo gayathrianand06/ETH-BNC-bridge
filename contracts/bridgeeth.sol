8f5e198f32d7e59cf93ca747ec9e73cab8c53933
gayathrianand06 <43163480+gayathrianand06@users.noreply.github.com>
2021-05-19 11:31:26 +0530

    Update bridgeeth.sol

--- contracts/bridgeeth.sol
+++ contracts/bridgeeth.sol
@@ -2,6 +2,6 @@ pragma solidity ^0.8.0;
 
 import './bridgebase.sol';
 
-contract BridgeEth is BridgeBase {
-  constructor(address token) BridgeBase(token) {}
+contract BridgeEth is bridgebase {
+  constructor(address token) bridgebase(token) {}
 }
