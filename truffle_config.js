1b26c3b99556e9ed4ae2dd9f08246edb9062bea4
gayathrianand06 <43163480+gayathrianand06@users.noreply.github.com>
2021-05-19 16:46:06 +0530

    Create truffle_config.js

---
+++ truffle_config.js
@@ -0,0 +1,69 @@
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const mnemonic = '';
+module.exports = {
+  /**
+   * $ truffle test --network <network-name>
+   */
+
+  networks: {
+    // development: {
+    //  host: "127.0.0.1",     // Localhost (default: none)
+    //  port: 8545,            // Standard Ethereum port (default: none)
+    //  network_id: "*",       // Any network (default: none)
+    // },
+    // Another network with more advanced options...
+    // advanced: {
+    // port: 8777,             // Custom port
+    // network_id: 1342,       // Custom network
+    // gas: 8500000,           // Gas sent with each transaction (default: ~6700000)
+    // gasPrice: 20000000000,  // 20 gwei (in wei) (default: 100 gwei)
+    // from: <address>,        // Account to send txs from (default: accounts[0])
+    // websockets: true        // Enable EventEmitter interface for web3 (default: false)
+    // },
+    // Useful for deploying to a public network.
+    // NB: It's important to wrap the provider as a function.
+    ethTestnet: {
+      provider: () => new HDWalletProvider(
+        mnemonic, 
+        'Infura Rinkeby url';
+        0,
+        1
+      ),
+      network_id: 4, //rinkeby
+      skipDryRun: true
+    },
+    bscTestnet: {
+      provider: () => new HDWalletProvider(
+        mnemonic, 
+        'https://data-seed-prebsc-1-s1.binance.org:8545'
+      ),
+      network_id: 97,
+      skipDryRun: true
+    }
+    // private: {
+    // provider: () => new HDWalletProvider(mnemonic, `https://network.io`),
+    // network_id: 2111,   // This network is yours, in the cloud.
+    // production: true    // Treats this network as if it was a public net. (default: false)
+    // }
+  },
+
+  // Set default mocha options here, use special reporters etc.
+  mocha: {
+    // timeout: 100000
+  },
+
+  // Configure your compilers
+  compilers: {
+    solc: {
+      version: "0.8.0",    
+      // docker: true,        
+      // settings: {         
+      //  optimizer: {
+      //    enabled: false,
+      //    runs: 200
+      //  },
+      //  evmVersion: "byzantium"
+      // }
+    }
+  }
+};
