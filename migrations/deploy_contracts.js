544c1d6ad96e4071edf9a45c741704bb54fc6ffb
gayathrianand06 <43163480+gayathrianand06@users.noreply.github.com>
2021-05-19 11:39:05 +0530

    Create deploy_contracts.js

---
+++ migrations/deploy_contracts.js
@@ -0,0 +1,22 @@
+const TokenEth = artifacts.require('tokeneth.sol');
+const TokenBsc = artifacts.require('tokenbsc.sol');
+const BridgeEth = artifacts.require('bridgeeth.sol');
+const BridgeBsc = artifacts.require('bridgebsc.sol');
+
+module.exports = async function (deployer, network, addresses) {
+  if(network === 'ethTestnet') {
+    await deployer.deploy(TokenEth);
+    const tokenEth = await TokenEth.deployed();
+    await tokenEth.mint(addresses[0], 1000);
+    await deployer.deploy(BridgeEth, tokenEth.address);
+    const bridgeEth = await BridgeEth.deployed();
+    await tokenEth.updateAdmin(bridgeEth.address);
+  }
+  if(network === 'bscTestnet') {
+    await deployer.deploy(TokenBsc);
+    const tokenBsc = await TokenBsc.deployed();
+    await deployer.deploy(BridgeBsc, tokenBsc.address);
+    const bridgeBsc = await BridgeBsc.deployed();
+    await tokenBsc.updateAdmin(bridgeBsc.address);
+  }
+};
